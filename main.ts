9ce13e9517ddc8e99e476e36a810b4df5c717cf1
Alejandro-roboprok <80068974+Alejandro-roboprok@users.noreply.github.com>
2021-03-16 08:43:09 -0300

    Actualizar pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace lectorIR{
+//%block="mostrar codigo IR"
+
 makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
 basic.showLeds(`
     . . . . .
@@ -10,3 +13,4 @@ basic.pause(1000)
 basic.forever(function () {
     basic.showNumber(makerbit.irButton())
 })
+}
