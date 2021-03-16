e627d3a822ea998d5e3fcb127b7a30ad415f298b
Alejandro-roboprok <80068974+Alejandro-roboprok@users.noreply.github.com>
2021-03-16 08:32:56 -0300

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
+basic.showLeds(`
+    . . . . .
+    # . # . #
+    # . # # .
+    # . # . .
+    # . # . .
+    `)
+basic.pause(1000)
 basic.forever(function () {
-	
+    basic.showNumber(makerbit.irButton())
 })
